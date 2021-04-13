c5f2951580d7d06b78a01c6c0f4651df84214e7a
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-04-13 13:30:02 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ExpenseItem =() =>{
+    return (
+        <div>
+           <h2>Expense item! </h2> 
+           {/* <h3>{Date.now}</h3> */}
+        </div>
+    )
+}
+
+export default ExpenseItem
